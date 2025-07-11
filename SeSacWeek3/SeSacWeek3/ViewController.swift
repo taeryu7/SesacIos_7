c76fdf370cf5bcaa9a2263c90a04f5e8087f68e9
taeryu7 <taeryu97@gmail.com>
2025-07-11 11:29:21 +0900

    git Test

--- SeSacWeek3/SeSacWeek3/ViewController.swift
+++ SeSacWeek3/SeSacWeek3/ViewController.swift
@@ -16,6 +16,12 @@ class ViewController: UIViewController {
         print(3)
         print(4)
         print(5)
+        
+        print("test")
+        
+        // 아 왜 다른건 다되는데 이건 안되냐
+        // 깃크라켄이랑 소스트리는 잘된다
+        // 애플은 각성하라
     }
 
 
