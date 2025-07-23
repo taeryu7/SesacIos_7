b8df10daf9cd6b2152f6b9bd90bc8f4cf661ce60
taeryu7 <taeryu97@gmail.com>
2025-07-24 06:21:46 +0900

    라벨 추가및 구조체 연결

--- SeSac20250723Report/SeSac20250723Report/Movie/MovieVC.swift
+++ SeSac20250723Report/SeSac20250723Report/Movie/MovieVC.swift
@@ -14,24 +14,50 @@ class MovieVC: UIViewController {
     let moviesearchBar = UISearchBar()
     let searchButton = UIButton()
     
+    var displayedMovies: [Movie] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureHierarchy()
         configureUView()
         configureLayout()
+        loadRandomMovies()
+    }
+    
+    func loadRandomMovies() {
+        displayedMovies = Array(MovieInfo.movies.shuffled().prefix(10))
+        tableView.reloadData()
     }
 }
 
-extension MovieVC: UITableViewDelegate, UITableViewDataSource {
+extension MovieVC: UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return displayedMovies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: MovieVCTableViewCell.identifier, for: indexPath) as! MovieVCTableViewCell
+        
+        let movie = displayedMovies[indexPath.row]
+        cell.numberLabel.text = "\(indexPath.row + 1)"
+        cell.titleLabel.text = movie.title
+        cell.dateLabel.text = formatDate(movie.releaseDate)
+        
         return cell
+    }
+    
+    func formatDate(_ dateString: String) -> String {
+        let year = String(dateString.prefix(4))
+        let month = String(dateString.dropFirst(4).prefix(2))
+        let day = String(dateString.suffix(2))
+        return "\(year)-\(month)-\(day)"
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        loadRandomMovies()
     }
     
     func configureHierarchy() {
@@ -68,6 +94,7 @@ extension MovieVC: UITableViewDelegate, UITableViewDataSource {
         // 서치바 설정
         moviesearchBar.placeholder = "영화를 검색해보세요"
         moviesearchBar.searchBarStyle = .minimal
+        moviesearchBar.delegate = self
         
         // 검색 버튼 설정
         searchButton.setTitle("검색", for: .normal)
@@ -75,6 +102,7 @@ extension MovieVC: UITableViewDelegate, UITableViewDataSource {
         searchButton.setTitleColor(.white, for: .normal)
         searchButton.layer.cornerRadius = 8
         searchButton.titleLabel?.font = .systemFont(ofSize: 16)
+        searchButton.addTarget(self, action: #selector(searchButtonTapped), for: .touchUpInside)
         
         // 테이블뷰 설정
         tableView.backgroundColor = .orange
@@ -82,6 +110,11 @@ extension MovieVC: UITableViewDelegate, UITableViewDataSource {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(MovieVCTableViewCell.self, forCellReuseIdentifier: MovieVCTableViewCell.identifier)
+    }
+    
+    @objc func searchButtonTapped() {
+        moviesearchBar.resignFirstResponder()
+        loadRandomMovies()
     }
 }
 
--- SeSac20250723Report/SeSac20250723Report/Movie/MovieVCTableViewCell.swift
+++ SeSac20250723Report/SeSac20250723Report/Movie/MovieVCTableViewCell.swift
@@ -12,13 +12,15 @@ class MovieVCTableViewCell: UITableViewCell {
     
     static let identifier = "MovieVCTableViewCell"
     
-    let numberLable = UILabel()
+    let numberLabel = UILabel()
+    let titleLabel = UILabel()
+    let dateLabel = UILabel()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         configureHierarchy()
-        configureView()
+        configureUView()
         configureLayout()
     }
     
@@ -30,17 +32,46 @@ class MovieVCTableViewCell: UITableViewCell {
 extension MovieVCTableViewCell {
     
     func configureHierarchy() {
-        contentView.addSubview(numberLable)
+        contentView.addSubview(numberLabel)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(dateLabel)
     }
     
-    func configureView() {
-        numberLable.snp.makeConstraints { make in
-            make.edges.equalTo(contentView).inset(10)
+    func configureUView() {
+        numberLabel.snp.makeConstraints { make in
+            make.leading.equalTo(contentView).offset(15)
+            make.centerY.equalTo(contentView)
+            make.width.equalTo(40)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(numberLabel.snp.trailing).offset(15)
+            make.trailing.equalTo(dateLabel.snp.leading).offset(-15)
+            make.centerY.equalTo(contentView)
+        }
+        
+        dateLabel.snp.makeConstraints { make in
+            make.trailing.equalTo(contentView).offset(-15)
+            make.centerY.equalTo(contentView)
+            make.width.equalTo(80)
         }
     }
     
     func configureLayout() {
-        numberLable.backgroundColor = .white
+        backgroundColor = .white
         
+        // 숫자 라벨 설정
+        numberLabel.textAlignment = .center
+        numberLabel.font = .systemFont(ofSize: 16, weight: .bold)
+        numberLabel.textColor = .systemBlue
+        
+        // 제목 라벨 설정
+        titleLabel.font = .systemFont(ofSize: 16)
+        titleLabel.textColor = .black
+        
+        // 날짜 라벨 설정
+        dateLabel.textAlignment = .right
+        dateLabel.font = .systemFont(ofSize: 14)
+        dateLabel.textColor = .systemGray
     }
 }
