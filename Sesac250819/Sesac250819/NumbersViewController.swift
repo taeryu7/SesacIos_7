6940e56fe2657539f2354181f5575dc71f1a9a0f
taeryu7 <taeryu97@gmail.com>
2025-08-19 20:06:49 +0900

    파일추가 및 스냅킷 적용

---
+++ Sesac250819/Sesac250819/NumbersViewController.swift
@@ -0,0 +1,134 @@
+//
+//  NumbersViewController.swift
+//  Sesac250819
+//
+//  Created by 유태호 on 8/19/25.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+class NumbersViewController: UIViewController {
+
+    let number1TextField = UITextField()
+    let number2TextField = UITextField()
+    let number3TextField = UITextField()
+    let plusLabel = UILabel()
+    let separatorView = UIView()
+    let resultLabel = UILabel()
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        title = "Adding numbers"
+        
+        configureLayout()
+        configure()
+        bindTextFields()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            target: self,
+            action: #selector(dismissViewController)
+        )
+    }
+    
+    @objc func dismissViewController() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func bindTextFields() {
+        Observable.combineLatest(
+            number1TextField.rx.text.orEmpty,
+            number2TextField.rx.text.orEmpty,
+            number3TextField.rx.text.orEmpty
+        ) { textValue1, textValue2, textValue3 -> Int in
+            return (Int(textValue1) ?? 0) + (Int(textValue2) ?? 0) + (Int(textValue3) ?? 0)
+        }
+        .map { $0.description }
+        .bind(to: resultLabel.rx.text)
+        .disposed(by: disposeBag)
+    }
+
+    func configure() {
+        number1TextField.text = "1"
+        number1TextField.borderStyle = .roundedRect
+        number1TextField.textAlignment = .right
+        number1TextField.keyboardType = .numberPad
+        number1TextField.font = .systemFont(ofSize: 14)
+        
+        number2TextField.text = "2"
+        number2TextField.borderStyle = .roundedRect
+        number2TextField.textAlignment = .right
+        number2TextField.keyboardType = .numberPad
+        number2TextField.font = .systemFont(ofSize: 14)
+        
+        number3TextField.text = "3"
+        number3TextField.borderStyle = .roundedRect
+        number3TextField.textAlignment = .right
+        number3TextField.keyboardType = .numberPad
+        number3TextField.font = .systemFont(ofSize: 14)
+        
+        plusLabel.text = "+"
+        plusLabel.font = .systemFont(ofSize: 17)
+        plusLabel.textColor = .label
+        
+        separatorView.backgroundColor = .systemGray3
+        
+        resultLabel.text = "-1"
+        resultLabel.textAlignment = .right
+        resultLabel.font = .systemFont(ofSize: 17)
+        resultLabel.textColor = .label
+    }
+    
+    func configureLayout() {
+        view.addSubview(number1TextField)
+        view.addSubview(number2TextField)
+        view.addSubview(number3TextField)
+        view.addSubview(plusLabel)
+        view.addSubview(separatorView)
+        view.addSubview(resultLabel)
+        
+        number3TextField.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.equalTo(97)
+            make.height.equalTo(30)
+        }
+        
+        number2TextField.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(number3TextField.snp.top).offset(-8)
+            make.width.equalTo(97)
+            make.height.equalTo(30)
+        }
+        
+        number1TextField.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(number2TextField.snp.top).offset(-8)
+            make.width.equalTo(97)
+            make.height.equalTo(30)
+        }
+        
+        plusLabel.snp.makeConstraints { make in
+            make.trailing.equalTo(number3TextField.snp.leading).offset(-8)
+            make.centerY.equalTo(number3TextField)
+        }
+        
+        separatorView.snp.makeConstraints { make in
+            make.top.equalTo(number3TextField.snp.bottom).offset(8)
+            make.leading.equalTo(plusLabel)
+            make.trailing.equalTo(number3TextField)
+            make.height.equalTo(1)
+        }
+        
+        resultLabel.snp.makeConstraints { make in
+            make.top.equalTo(separatorView.snp.bottom).offset(8)
+            make.leading.equalTo(separatorView)
+            make.trailing.equalTo(separatorView)
+        }
+    }
+}
--- Sesac250819/Sesac250819/ViewController.swift
+++ Sesac250819/Sesac250819/ViewController.swift
@@ -8,12 +8,80 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let numbersViewButton = UIButton(type: .system)
+    private let simpleTableViewButton = UIButton(type: .system)
+    private let simpleValidationViewButton = UIButton(type: .system)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        setupConstraints()
     }
-
-
+    
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+        
+        numbersViewButton.setTitle("NumbersView", for: .normal)
+        numbersViewButton.titleLabel?.font = .systemFont(ofSize: 18, weight: .medium)
+        numbersViewButton.backgroundColor = .systemBlue
+        numbersViewButton.setTitleColor(.white, for: .normal)
+        numbersViewButton.layer.cornerRadius = 8
+        numbersViewButton.addTarget(self, action: #selector(numbersViewButtonTapped), for: .touchUpInside)
+        
+        simpleTableViewButton.setTitle("SimpleTableView", for: .normal)
+        simpleTableViewButton.titleLabel?.font = .systemFont(ofSize: 18, weight: .medium)
+        simpleTableViewButton.backgroundColor = .systemGreen
+        simpleTableViewButton.setTitleColor(.white, for: .normal)
+        simpleTableViewButton.layer.cornerRadius = 8
+        simpleTableViewButton.addTarget(self, action: #selector(simpleTableViewButtonTapped), for: .touchUpInside)
+        
+        simpleValidationViewButton.setTitle("SimpleValidationView", for: .normal)
+        simpleValidationViewButton.titleLabel?.font = .systemFont(ofSize: 18, weight: .medium)
+        simpleValidationViewButton.backgroundColor = .systemOrange
+        simpleValidationViewButton.setTitleColor(.white, for: .normal)
+        simpleValidationViewButton.layer.cornerRadius = 8
+        simpleValidationViewButton.addTarget(self, action: #selector(simpleValidationViewButtonTapped), for: .touchUpInside)
+        
+        view.addSubview(numbersViewButton)
+        view.addSubview(simpleTableViewButton)
+        view.addSubview(simpleValidationViewButton)
+        
+        numbersViewButton.translatesAutoresizingMaskIntoConstraints = false
+        simpleTableViewButton.translatesAutoresizingMaskIntoConstraints = false
+        simpleValidationViewButton.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            numbersViewButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            numbersViewButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -80),
+            numbersViewButton.widthAnchor.constraint(equalToConstant: 200),
+            numbersViewButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            simpleTableViewButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            simpleTableViewButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            simpleTableViewButton.widthAnchor.constraint(equalToConstant: 200),
+            simpleTableViewButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            simpleValidationViewButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            simpleValidationViewButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 80),
+            simpleValidationViewButton.widthAnchor.constraint(equalToConstant: 200),
+            simpleValidationViewButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    @objc private func numbersViewButtonTapped() {
+        let numbersViewController = NumbersViewController()
+        let navigationController = UINavigationController(rootViewController: numbersViewController)
+        present(navigationController, animated: true, completion: nil)
+    }
+    
+    @objc private func simpleTableViewButtonTapped() {
+        print("SimpleTableView 버튼이 눌렸습니다")
+    }
+    
+    @objc private func simpleValidationViewButtonTapped() {
+        print("SimpleValidationView 버튼이 눌렸습니다")
+    }
 }
-
