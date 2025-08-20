beaeff0fff90da8afbc3a572deb09d3a110416e7
taeryu7 <taeryu97@gmail.com>
2025-08-20 16:10:52 +0900

    RX 프로젝트 세팅

---
+++ Sesac250820/Sesac250820/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sesac250820
+//
+//  Created by 유태호 on 8/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
